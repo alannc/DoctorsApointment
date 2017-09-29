c1d00b6681b56d2f39c2b609d41f4afbdbb103d0
Alan Castro <alannnc@gmail.com>
2017-09-28 19:04:11 -0500

    First Commit

---
+++ app/src/main/java/com/alannnc/apointments/hospitalapointments/MainActivity.java
@@ -0,0 +1,73 @@
+package com.alannnc.apointments.hospitalapointments;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int RESULT_LOAD = 1;
+    String img_Decodable_Str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadImagefromGallery(View view) {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                img_Decodable_Str = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imgView1);
+                // Set the Image in ImageView after decoding the String
+                Log.d("IMAGE", "IMAGE IS SETTING");
+                imgView.setImageURI(selectedImage);
+
+                //imgView.setImageBitmap(BitmapFactory
+                //        .decodeFile(img_Decodable_Str));
+
+            } else {
+                Toast.makeText(this, "Hey pick your image first",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went embrassing", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+}
